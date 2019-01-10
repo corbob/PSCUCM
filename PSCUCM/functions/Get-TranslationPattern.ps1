ddc9a2d7a5abd4e956851946d4ca42a0e6106c0f
corbob <corbobgithub@knoxy.ca>
2019-01-09 19:51:35 -0800

    Add Translation Pattern Stuffs....

---
+++ PSCUCM/functions/Get-TranslationPattern.ps1
@@ -0,0 +1,63 @@
+function Get-TranslationPattern {
+    <#
+    .SYNOPSIS
+    Get Translation Patten
+    
+    .DESCRIPTION
+    Get Translation Pattern
+    
+    .PARAMETER TranslationPattern
+    Translation Pattern to get
+    
+    .PARAMETER RoutePartitionName
+    Route Partition that the Translation Pattern is a part of.
+    
+    .PARAMETER server
+    Server to query
+    
+    .PARAMETER Credential
+    Credential to use for API access
+    
+    .PARAMETER EnableException
+    Replaces user friendly yellow warnings with bloody red exceptions of doom!
+    Use this if you want the function to throw terminating errors you want to catch.
+    
+    .PARAMETER OutputXml
+    Enable the output of the XML instead of the processing of the entity.
+    
+    .EXAMPLE
+    An example
+
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $TranslationPattern,
+        [string]
+        $RoutePartitionName,
+        [Parameter(Mandatory = $true)]
+        [string]
+        $server,
+        [Parameter(Mandatory = $true)]
+        [pscredential]
+        $Credential,
+        [switch]
+        $EnableException,
+        [switch]
+        $OutputXml
+    )
+    $invokeCucmAxlSplat = @{
+        entity              = 'getTransPattern'
+        parameters = @{
+            pattern = $TranslationPattern
+            routePartitionName = $RoutePartitionName
+        }
+        server          = $server
+        Credential      = $Credential
+        EnableException = $EnableException
+        OutputXml       = $OutputXml
+    }
+    Invoke-CucmAxl @invokeCucmAxlSplat
+    
+}
---
+++ PSCUCM/functions/Set-TranslationPattern.ps1
@@ -0,0 +1,74 @@
+function Set-TranslationPattern {
+    <#
+    .SYNOPSIS
+    Set Translation Patten
+    
+    .DESCRIPTION
+    Set Translation Pattern
+    
+    .PARAMETER TranslationPattern
+    Translation Pattern to set
+    
+    .PARAMETER RoutePartitionName
+    Route Partition that the Translation Pattern is a part of.
+    
+    .PARAMETER calledPartyTransformationMask
+    Transformation Mask
+    
+    .PARAMETER server
+    Server to query
+    
+    .PARAMETER Credential
+    Credential to use for API access
+    
+    .PARAMETER EnableException
+    Replaces user friendly yellow warnings with bloody red exceptions of doom!
+    Use this if you want the function to throw terminating errors you want to catch.
+    
+    .PARAMETER OutputXml
+    Enable the output of the XML instead of the processing of the entity.
+    
+    .EXAMPLE
+    An example
+
+    #>
+    
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $TranslationPattern,
+        [string]
+        $RoutePartitionName,
+        [string]
+        $calledPartyTransformationMask,
+        [Parameter(Mandatory = $true)]
+        [string]
+        $server,
+        [Parameter(Mandatory = $true)]
+        [pscredential]
+        $Credential,
+        [switch]
+        $EnableException,
+        [switch]
+        $OutputXml
+    )
+    $invokeCucmAxlSplat = @{
+        entity          = 'updateTransPattern'
+        parameters      = @{
+            pattern            = $TranslationPattern
+        }
+        server          = $server
+        Credential      = $Credential
+        EnableException = $EnableException
+        OutputXml       = $OutputXml
+    }
+    if($null -ne $RoutePartitionName) {
+        $invokeCucmAxlSplat.parameters.routePartitionName = $RoutePartitionName
+    }
+    if($null -ne $calledPartyTransformationMask) {
+        $invokeCucmAxlSplat.parameters.calledPartyTransformationMask = $calledPartyTransformationMask
+    }
+    Invoke-CucmAxl @invokeCucmAxlSplat
+   
+}
