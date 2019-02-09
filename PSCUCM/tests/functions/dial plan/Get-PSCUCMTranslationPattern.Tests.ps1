c3d9c2ca141ab9191b9ff65a3fee93a8fb589213
CorBob <corbob@knoxy.ca>
2019-02-08 23:08:09 -0800

    More tests added. Cleaning up failing functions

---
+++ PSCUCM/tests/functions/dial plan/Get-PSCUCMTranslationPattern.Tests.ps1
@@ -0,0 +1,42 @@
+﻿Import-Module $PSScriptRoot\..\..\..\PSCUCM.psd1 -Force
+
+Describe "Get-PSCUCMTranslationPattern" {
+    BeforeAll {
+        [xml]$AxlReturn = @'
+        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns="http://www.cisco.com/AXL/API/11.5">
+        <soapenv:Header/>
+        <soapenv:Body>
+            <ns:getTransPattern>
+                <pattern>lkj</pattern><routePartitionName></routePartitionName>
+            </ns:getTransPattern>
+        </soapenv:Body>
+    </soapenv:Envelope>
+'@
+        [xml]$RoutePartitionNameAxlReturn = @'
+        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns="http://www.cisco.com/AXL/API/11.5">
+        <soapenv:Header/>
+        <soapenv:Body>
+            <ns:getTransPattern>
+                <pattern>lkj</pattern><routePartitionName>RoutePartition</routePartitionName>
+            </ns:getTransPattern>
+        </soapenv:Body>
+    </soapenv:Envelope>
+'@
+        $getPSCUCMTranslationPatternSplat = @{
+            TranslationPattern = 'lkj'
+        }
+    }
+    It "Returns appropriate XML for Starting a sync" {
+        [xml]$return = Get-PSCUCMTranslationPattern @getPSCUCMTranslationPatternSplat -OutputXML
+        $return.OuterXml | Should -Be $AxlReturn.OuterXml
+    }
+    It "Returns appropriate XML for cancelling a sync" {
+        [xml]$return = Get-PSCUCMTranslationPattern @getPSCUCMTranslationPatternSplat -OutputXML -RoutePartitionName "RoutePartition"
+        $return.OuterXml | Should -Be $RoutePartitionNameAxlReturn.OuterXml
+    }
+    It "Calls Invoke-PSCUCMAxlQuery" {
+        Mock -CommandName Invoke-PSCUCMAxlQuery -MockWith {} -ModuleName PSCUCM
+        Get-PSCUCMTranslationPattern @getPSCUCMTranslationPatternSplat
+        Assert-MockCalled -CommandName Invoke-PSCUCMAxlQuery -Times 1 -Exactly -ModuleName PSCUCM
+    }
+}
---
+++ PSCUCM/tests/functions/system/Invoke-PSCUCMLdapSync.Tests.ps1
@@ -0,0 +1,42 @@
+﻿Import-Module $PSScriptRoot\..\..\..\PSCUCM.psd1 -Force
+
+Describe "Invoke-PSCUCMLdapSync" {
+    BeforeAll {
+        [xml]$AxlReturn = @'
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns="http://www.cisco.com/AXL/API/11.5">
+    <soapenv:Header/>
+    <soapenv:Body>
+    <ns:doLdapSync>
+        <sync>True</sync><name>lkj</name>
+    </ns:doLdapSync>
+    </soapenv:Body>
+</soapenv:Envelope>
+'@
+        [xml]$CancelAxlReturn = @'
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns="http://www.cisco.com/AXL/API/11.5">
+    <soapenv:Header/>
+    <soapenv:Body>
+        <ns:doLdapSync>
+            <sync>False</sync><name>lkj</name>
+        </ns:doLdapSync>
+    </soapenv:Body>
+</soapenv:Envelope>
+'@
+        $invokePSCUCMLdapSyncSplat = @{
+            LdapDirectory = 'lkj'
+        }
+    }
+    It "Returns appropriate XML for Starting a sync" {
+        [xml]$return = Invoke-PSCUCMLdapSync @invokePSCUCMLdapSyncSplat -OutputXML
+        $return.OuterXml | Should -Be $AxlReturn.OuterXml
+    }
+    It "Returns appropriate XML for cancelling a sync" {
+        [xml]$return = Invoke-PSCUCMLdapSync @invokePSCUCMLdapSyncSplat -OutputXML -CancelActive
+        $return.OuterXml | Should -Be $CancelAxlReturn.OuterXml
+    }
+    It "Calls Invoke-PSCUCMAxlQuery" {
+        Mock -CommandName Invoke-PSCUCMAxlQuery -MockWith {} -ModuleName PSCUCM
+        Invoke-PSCUCMLdapSync @invokePSCUCMLdapSyncSplat
+        Assert-MockCalled -CommandName Invoke-PSCUCMAxlQuery -Times 1 -Exactly -ModuleName PSCUCM
+    }
+}
