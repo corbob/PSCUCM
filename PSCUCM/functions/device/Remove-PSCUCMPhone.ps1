1f6970398917caf6d297b074aebe75828df48edc
corbob <corbob@knoxy.ca>
2020-10-03 14:39:04 -0700

    Add Help

--- PSCUCM/functions/device/Remove-PSCUCMPhone.ps1
+++ PSCUCM/functions/device/Remove-PSCUCMPhone.ps1
@@ -1,4 +1,32 @@
 ﻿function Remove-PSCUCMPhone {
+    <#
+    .SYNOPSIS
+    Remove a phone from CUCM
+    
+    .DESCRIPTION
+    Remove a phoen from CUCM
+    
+    .PARAMETER Name
+    Name of phone to remove
+    
+    .PARAMETER UUID
+    UUID of phone to remove
+    
+    .PARAMETER EnableException
+    Parameter description
+
+    .PARAMETER WhatIf
+    What If?
+    
+    .PARAMETER Confirm
+    Confirm...
+    
+    .EXAMPLE
+    Remove-PSCUCMPhone -UUID {3C33ADA6-70DF-F16C-C5B9-16CE14AE58B3}
+
+    Removes phone with UUID {3C33ADA6-70DF-F16C-C5B9-16CE14AE58B3}
+
+    #>
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "Low")]
     param (
         [Parameter(ParameterSetName = "Name", Mandatory)]
