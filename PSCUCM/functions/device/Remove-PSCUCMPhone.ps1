2b817affecd4b706db1a855f4a95dd886d9b0540
corbob <corbob@knoxy.ca>
2020-10-03 13:29:57 -0700

    Add initial file

---
+++ PSCUCM/functions/device/Remove-PSCUCMPhone.ps1
@@ -0,0 +1,11 @@
+﻿function Remove-PSCUCMPhone {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $DN,
+        [switch]
+        $EnableException
+    )
+    Invoke-PSCUCMAxlQuery @CucmAxlSplat | Select-Xml -XPath '//phone' | Select-Object -ExpandProperty node
+}
